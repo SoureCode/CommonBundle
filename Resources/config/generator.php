7c3fbf916fa06b03705c1c0ccbdef3f35a868ffd
chapterjason <jason@sourecode.dev>
2021-05-11 18:19:31 +0200

    cut: Deprecate generator service

--- Resources/config/generator.php
+++ Resources/config/generator.php
@@ -12,6 +12,16 @@ return function (ContainerConfigurator $container) {
     // ============================
     // = Generator
     // ============================
-    $services->set('sourecode.common.generator.random', RandomGenerator::class);
-    $services->alias(RandomGeneratorInterface::class, 'sourecode.common.generator.random');
+    $services->set('sourecode.common.generator.random', RandomGenerator::class)
+        ->deprecate(
+            'SoureCode/CommonBundle',
+            '0.2.0',
+            'Service sourecode.common.generator.random is deprecated since 0.2.0 and will be removed in 1.0.0.'
+        );
+    $services->alias(RandomGeneratorInterface::class, 'sourecode.common.generator.random')
+        ->deprecate(
+            'SoureCode/CommonBundle',
+            '0.2.0',
+            'Service '.RandomGeneratorInterface::class.' is deprecated since 0.2.0 and will be removed in 1.0.0.'
+        );
 };
